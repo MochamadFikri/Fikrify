e8619b43fdbfb5ca591ddec53850e994b81bb52f
MochamadFikri <mochamadfikri82@gmail.com>
2020-05-09 23:00:22 +0700

    Just normal Initial Commit

---
+++ app/src/main/java/com/example/fikrify/Home.kt
@@ -0,0 +1,12 @@
+package com.example.fikrify
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_home)
+    }
+}
---
+++ app/src/main/java/com/example/fikrify/Splash_Screen.kt
@@ -0,0 +1,24 @@
+package com.example.fikrify
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splash_Screen : AppCompatActivity() {
+
+    lateinit var handler:Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_splash_screen)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@Splash_Screen,Home::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+
+}
